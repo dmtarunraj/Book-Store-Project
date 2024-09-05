eaaa8303d7a5e75605955d71a781d2685d8604cc
dmtarunraj <dmtarunraj117@gmail.com>
2024-09-05 21:54:27 +0530

    Backend

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+
+export const mongoDBURL =
+  'mongodb+srv://dmtarunraj:tarun123@cluster0.0jvvwsw.mongodb.net/Book_Collection?retryWrites=true&w=majority&appName=Cluster0';
